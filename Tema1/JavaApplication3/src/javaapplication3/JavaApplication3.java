7f18c39d5d4620367210892f09f4629df5e04272
Pablo <instypablo@gmail.com>
2022-10-05 20:24:15 +0200

    Mitad de la mitad de la actividad

--- Tema1/JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ Tema1/JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -3,13 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package javaapplication3;
-public class JavaApplication3 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import java.io.File;
+
+public class JavaApplication3 {
+    private void Lanzador(Integer num1, Integer num2, String file){
+        ProcessBuilder pb;
+        String clase = "C:\\Users\\usuario\\Desktop\\2DAM\\P-S-P\\EjemploThreads\\src\\ejemplothreads\\Sumatorio.java";
+        try {
+            pb = new ProcessBuilder("java",clase,num1.toString(),num2.toString());
+            pb.redirectError(new File("error.txt"));
+            pb.redirectOutput(new File(file));
+        } catch (Exception e) {
+            System.out.println("Error1");
+        }
     }
-    
+    public static void main(String[] args) {
+        
+    }
 }
